66b0584a56c0059a3b118400ee0fba79ac6c0f9b
Dzmitry Sazonov <dzmitry.sazonau@gmail.com>
2021-01-18 20:12:12 +0300

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,6 +37,7 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/semi': 'off',
     'no-console': 'off',
-    'import/no-cycle': 'off'
+    'import/no-cycle': 'off',
+    'no-confusing-arrow': 'off'
   },
 };
